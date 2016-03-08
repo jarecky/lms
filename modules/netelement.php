3dd6ba6946b950fcc18ccaf8fc7a7c7dcde5755d
ernesttar <ernest@poczta.tarman.pl>
2016-03-08 15:49:07 +0100

    ernest 	modified:   modules/netelement.php
    	zmiana na netelement +akcje
    	deleted:    modules/netelementadd.php
    
    	poprawki szablonow + obsluga akcji
    	modified:   templates/default/netelements/netelementadd.html
    	new file:   templates/default/netelements/netelementinfo.html
    	new file:   templates/default/netelements/netelementlist.html

--- modules/netelement.php
+++ modules/netelement.php
@@ -1,5 +1,4 @@
 <?php
-$layout['pagetitle'] = trans('New Device <i>example</i>');
 
 //funkcje wlasne
 $producers = $DB->GetAll('
@@ -33,22 +32,45 @@ function select_model($id, $type){
 
 }
 
-
-
-
-$nprojects = $DB->GetAll("SELECT * FROM invprojects WHERE type<>? ORDER BY name", array(INV_PROJECT_SYSTEM));
-$SMARTY->assign('NNprojects',$nprojects);
-$netnodes = $DB->GetAll("SELECT * FROM netnodes ORDER BY name");
-
 $LMS->InitXajax();
 $LMS->RegisterXajaxFunction('select_producer');
 $LMS->RegisterXajaxFunction('select_model');
 $SMARTY->assign('xajax', $LMS->RunXajax());
 
 
+$nprojects = $DB->GetAll("SELECT * FROM invprojects WHERE type<>? ORDER BY name", array(INV_PROJECT_SYSTEM));
+$SMARTY->assign('NNprojects',$nprojects);
+$netnodes = $DB->GetAll("SELECT * FROM netnodes ORDER BY name");
+
+
 $SMARTY->assign('NNnodes',$netnodes);
 $SMARTY->assign('producers', $producers);
+if(!isset($_GET['action'])) $_GET['action']='';
+print_r($_GET['action']);
+  switch ($_GET['action']){
+    case 'add':
+      $layout['pagetitle'] = trans('New Device');
+      $SMARTY->display("netelements/netelementadd.html");
+    break;
+    case 'edit':
+      $layout['pagetitle'] = trans('Device Edit: <b>$a</b> $b $c', $netdevinfo['name'], $netdevinfo['producer'], $netdevinfo['model']);;
+      if(!isset($_GET['id']) || !isset($_GET['type'])) header('Location: ?m=netelement');
+      //dodac asygnacje zmiennych
+      $SMARTY->display("netelements/netelementadd.html");
+    break;
+    case 'info':
+      if(!isset($_GET['id']) || !isset($_GET['type'])) header('Location: ?m=netelement');
+      $layout['pagetitle'] = trans('Device Info: <b>$a</b> $b $c', $netdevinfo['name'], $netdevinfo['producer'], $netdevinfo['model']);;
+      //dodac asygnacje zmiennych
+      $SMARTY->display("netelements/netelementinfo.html");
+      break;
+    default:
+    //lista urzadzen
+      $layout['pagetitle'] = trans('Elements list');
+      //dodac asygnacje zmiennych
+      $SMARTY->display("netelements/netelementlist.html");
+    break;
+    }
 
 
-$SMARTY->display("netelements/netelementadd.html");
 ?>
