d70aaf2fbe7e5b9a6e5e2ae3c40bbcd043db3a6f
ernesttar <ernest@poczta.tarman.pl>
2016-03-08 16:16:06 +0100

    ernest
    	modified:   modules/netelement.php
    	modified:   templates/default/netelements/netelementadd.html
    	modified:   templates/default/netelements/netelementlist.html

--- modules/netelement.php
+++ modules/netelement.php
@@ -46,28 +46,40 @@ $netnodes = $DB->GetAll("SELECT * FROM netnodes ORDER BY name");
 $SMARTY->assign('NNnodes',$netnodes);
 $SMARTY->assign('producers', $producers);
 if(!isset($_GET['action'])) $_GET['action']='';
-print_r($_GET['action']);
-  switch ($_GET['action']){
+
+switch ($_GET['action']){
     case 'add':
+      if(isset($_GET['save']) /*&& validacja*/){
+	$elem_id = $LMS->netElementsManager->netElementAdd($_GET['netelem']);
+	if(!isset($_GET['netelem[reuse]'])) header('Location: ?m=netelement&action=info&id='.$elem_id);
+      }
       $layout['pagetitle'] = trans('New Device');
       $SMARTY->display("netelements/netelementadd.html");
     break;
+
     case 'edit':
       $layout['pagetitle'] = trans('Device Edit: <b>$a</b> $b $c', $netdevinfo['name'], $netdevinfo['producer'], $netdevinfo['model']);;
       if(!isset($_GET['id']) || !isset($_GET['type'])) header('Location: ?m=netelement');
+//	$listdata = $LMS->netElementsManager->netElementsInfo($_GET['id'], $_GET['type'])      
       //dodac asygnacje zmiennych
       $SMARTY->display("netelements/netelementadd.html");
     break;
+
     case 'info':
       if(!isset($_GET['id']) || !isset($_GET['type'])) header('Location: ?m=netelement');
       $layout['pagetitle'] = trans('Device Info: <b>$a</b> $b $c', $netdevinfo['name'], $netdevinfo['producer'], $netdevinfo['model']);;
+//	$listdata = $LMS->netElementsManager->netElementsInfo($_GET['id'], $_GET['type'])      
       //dodac asygnacje zmiennych
       $SMARTY->display("netelements/netelementinfo.html");
       break;
-    default:
+
+      default:
     //lista urzadzen
-      $layout['pagetitle'] = trans('Elements list');
+      $layout['pagetitle'] = trans('Network elements list');
+//cos w ten desen ??
+//	$listdata = $LMS->netElementsManager->netElementsList($_GET['o'], $_GET['filter'])      
       //dodac asygnacje zmiennych
+//	$SMARTY->assign('listdata,$listdata');      
       $SMARTY->display("netelements/netelementlist.html");
     break;
     }
