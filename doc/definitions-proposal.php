caab0e97c1a7d500295d743ea7ab3a0f6a2862d9
ernesttar <ernest@poczta.tarman.pl>
2016-03-21 11:25:21 +0100

    new file:   doc/definitions-proposal.php

---
+++ doc/definitions-proposal.php
@@ -0,0 +1,11 @@
+$_CONNECTORS=array('SC/PC',
+		    'SC/APC',
+		    'LC' );
+$_SPEEDS=array('1/1', '2/2', '5/5', '10/10', '25/25', '54/54', '100/100', '1250/1250');
+$_NETTECHNOLOGIES=array(
+    array(	'name'=>'...', 
+		'speed_list'=>'0,1,2,4', 
+		'connector_list'=>'0,1'
+    ),
+
+);
