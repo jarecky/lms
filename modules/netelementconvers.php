b37b8456ae9778c277d3220263b67e514fa30b48
Jaroslaw Dziubek <yaro@perfect.net.pl>
2016-04-19 11:05:37 +0200

    .

--- modules/netelementconvers.php
+++ modules/netelementconvers.php
@@ -94,23 +94,105 @@ if (is_array($devices)) foreach ($devices AS $dev) {
 // Etap 1. - przeniesienie danych z netdevices_old do netelements
 // *******************************************************************
 
-$devices=$DB->GetAll("SELECT * FROM netdevices_old WHERE name like '%-ap' ORDER BY id ASC LIMIT 0,10");
+$devices=$DB->GetAll("SELECT * FROM netdevices_old WHERE name regexp '(ap|rt)$' ORDER BY id ASC LIMIT 0,10");
 if (is_array($devices)) foreach ($devices AS $dev) {
-	echo $dev['name'].' '.$dev['producer'].' '.$dev['model'].'<BR>';
-	$DB->Execute("INSERT INTO netelements VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",array(null,$dev['name'],0,$dev['description'],$dev['producer'],$dev['model'],$dev['serialnumber'],$dev['purchasetime'],$dev['guaranteeperiod'],$dev['netnodeid'],$dev['invprojectid'],$dev['netdevicemodelid'],$dev['status']));
-	$netelemid = $DB->GetLastInsertID('netelements');
-	$DB->Execute("INSERT INTO netdevices VALUES (?,?,?,?,?,?,?,?,?)",array(null,$netelemid,$dev['shortname'],$dev['nastype'],$dev['clients'],'',$dev['secret'],$dev['community'],$dev['channelid']));
-	echo '<font color="green">Ilość portów: '.$dev['ports'].'</fonts><BR>';
-	$nls=$DB->GetAll("SELECT * FROM netlinks WHERE src=".$dev['id']." OR dst=".$dev['id']);
-	if (is_array($nls)) foreach ($nls AS $ls) {
-		
-	}
-	$rss=$DB->GetAll("SELECT * FROM netradiosectors_old WHERE netdev=".$dev['id']);
-	if (is_array($rss)) foreach ($rss AS $rs) {
-		
-	}
+	$check=$DB->GetOne("SELECT id FROM netelements WHERE name='".$dev['name']."'");
+	if (!isset($check)) {
+		echo $dev['name'].' '.$dev['producer'].' '.$dev['model'].'<BR>';
+		#$DB->Execute("INSERT INTO netelements VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",array(null,$dev['name'],0,$dev['description'],$dev['producer'],$dev['model'],$dev['serialnumber'],$dev['purchasetime'],$dev['guaranteeperiod'],$dev['netnodeid'],$dev['invprojectid'],$dev['netdevicemodelid'],$dev['status']));
+		#$netelemid = $DB->GetLastInsertID('netelements');
+		#$DB->Execute("INSERT INTO netdevices VALUES (?,?,?,?,?,?,?,?,?)",array(null,$netelemid,$dev['shortname'],$dev['nastype'],$dev['clients'],'',$dev['secret'],$dev['community'],$dev['channelid']));
+		echo '<font color="green">Ilość portów: '.$dev['ports'].'</font><BR>';
+		$tech=array();
+		$nls=$DB->GetAll("SELECT * FROM netlinks WHERE src=".$dev['id']." OR dst=".$dev['id']);
+		if (is_array($nls)) foreach ($nls AS $ls) {
+			if ($ls['technology']>=100 and $ls['technology']<200) {
+				if ($ls['src']==$dev['id']) {
+					if ($ls['srcradiosector']) {
+						$tech[$ls['technology']][$ls['srcradiosector']]=1;
+					} else {
+						$tech[$ls['technology']][0]++;
+					}
+				} else {
+					if ($ls['dstradiosector']) {
+						$tech[$ls['technology']][$ls['dstradiosector']]=1;
+					} else {
+						$tech[$ls['technology']][0]++;
+					}
+				}
+			} else {
+				if ($ls['src']==$dev['id']) {
+					$tech[$ls['technology']][$ls['dstport']]++;
+				} else {
+					$tech[$ls['technology']][$ls['srcport']]++;
+				}
+			}
+		}
+		#echo '<PRE>';print_r($tech);echo '</PRE>';
+		$ports=array();
+		$wport_num=1;
+		if (is_array($tech)) foreach ($tech AS $t => $p ) {
+			if ($t<100) {
+				foreach ($p AS $nr => $i) {
+					if (preg_match('/^([0-9]+),?/',$NETTECHNOLOGIES[$t]['connector'],$c))
+						$connector=$c[1];
+					for ($x=1;$x<=$i;$x++) {
+						$port=array(
+							'id' 		=> NULL,
+							'netelemid'	=> $netelemid,
+							'type'		=> 0,
+							'label'		=> $nr ? 'copper'.$nr : 'copper'.$i,
+							'connectortype'	=> $connector,
+							'technology'	=> $t,
+							'capacity'	=> 1
+						);
+						$ports[]=$port;
+					}
+				}
+			} elseif ($t<200) {
+				foreach ($p AS $rs => $i) {
+					if ($rs>0) {
+						$rss=$DB->GetAll("SELECT * FROM netradiosectors_old WHERE id=".$rs);
+						$rss=$rss[0];
+						$port=array(
+                                                        'id'            => NULL,
+                                                        'netelemid'     => $netelemid,
+                                                        'type'          => 2,
+                                                        'label'         => 'wireless'.$wport_num++,
+                                                        'connectortype' => 0,
+                                                        'technology'    => $t,
+                                                        'capacity'      => 100,
+							'radiosector'	=> $rss
+                                                );
+						$ports[]=$port;
 
-	echo '<BR>';
+					} else { 
+						for ($x=1;$x<=$i;$x++) {
+							$port=array(
+								'id'            => NULL,
+								'netelemid'     => $netelemid,
+								'type'          => 2,
+								'label'         => 'wireless'.$port_num++,
+								'connectortype' => 0,
+								'technology'    => $t,
+								'capacity'      => 1
+							);
+							$ports[]=$port;
+						}
+					}
+				}
+
+			} elseif ($t<300) {
+	
+			} else {
+				echo '<FONT color="red">Nieznana technologia '.$t.'</FONT><BR>';
+			}
+		}
+		echo '<PRE>';print_r($ports);echo '</PRE>';
+		echo '<BR>';
+	} else {
+		echo '<FONT COLOR="red">Urządzenie '.$dev['name'].' juz zaimportowane!<BR>';
+	}
 }
 
 
