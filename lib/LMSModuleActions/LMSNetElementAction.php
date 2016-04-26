0dd90e7dc99814bbf0cbf6f32d643bc3cd73f194
ernesttar <ernest@poczta.tarman.pl>
2016-04-26 10:35:26 +0200

    ernest 	modified:   lib/LMSModuleActions/LMSNetElementAction.php 	modified:   modules/netelement.php 	modified:   templates/default/netelements/add.html

--- lib/LMSModuleActions/LMSNetElementAction.php
+++ lib/LMSModuleActions/LMSNetElementAction.php
@@ -233,8 +233,8 @@ class LMSNetElemAction extends LMSModuleAction
 			// PASYWNE
 			} elseif ($netelemdata['type']==2) {
 			// KABEL
-				echo '<PRE>';print_r($netcabledata);echo '</PRE>';
 				$netcabledata=$_POST['netcable'];
+				echo '<PRE>';print_r($netcabledata);echo '</PRE>';
 				if (!is_integer($netcabledata['distance']))
 					$error['distance']=trans('Distance must be integer number!');
 				if (!is_integer($netcabledata['capacity']))
