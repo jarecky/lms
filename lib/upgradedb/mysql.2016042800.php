6ab8f2cc735f35f01fda2017d4c7ff10323f7c16
ernesttar <ernest@poczta.tarman.pl>
2016-04-28 15:25:41 +0200

    ernest: dodalem pole technology do deviceschema 	modified:   lib/LMSDB_common.class.php 	new file:   lib/upgradedb/mysql.2016042800.php

---
+++ lib/upgradedb/mysql.2016042800.php
@@ -0,0 +1,12 @@
+<?php
+$this->BeginTrans();
+
+$this->Execute("SET foreign_key_checks = 0");
+
+$this->Execute("ALTER TABLE  `netdeviceschema` CHANGE  `portcount`  `technology` INT( 11 ) NULL");
+
+$this->Execute("UPDATE dbinfo SET keyvalue = ? WHERE keytype = ?", array('2016042800', 'dbversion'));
+
+$this->CommitTrans();
+
+
