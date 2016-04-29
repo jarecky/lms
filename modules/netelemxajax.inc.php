2ee0bebf1557536699f1119e3931a88edd4d7ef7
ernesttar <ernest@poczta.tarman.pl>
2016-04-29 12:01:39 +0200

    modified:   modules/netelemxajax.inc.php

--- modules/netelemxajax.inc.php
+++ modules/netelemxajax.inc.php
@@ -387,7 +387,7 @@ function changeNetElementType($type) {
 	$res->assign('elem_type_multiplexer','style.display', 'none');
 	$res->assign('elem_type_computer','style.display', 'none');
 	$res->assign('elem_ports','style.display', 'none');
-	if($type==-1 || !isset($_POST['netports']))$res->assign('porttable', 'innerHTML', '');
+	if($type==-1 || !empty($_POST['netports']))$res->assign('porttable', 'innerHTML', '');
 		  $q="SELECT distinct(p.id), p.name FROM netdeviceproducers p 
 		      LEFT JOIN netdevicemodels m ON p.id=m.netdeviceproducerid
 		      WHERE m.type=".$type;
